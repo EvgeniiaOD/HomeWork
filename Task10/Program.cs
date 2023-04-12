fbbd9a9a511a67c4a1a04af1a22d159c5d150915
Evgeniia <evgeniia.d@gmail.com>
2023-04-13 00:02:16 +0200

    закомментила одно решение, чтоб работал код

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -9,13 +9,13 @@
 Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number >= 100 && number <= 999)
-{
-    int secondDigit = number / 10;
-    int firstDigit = number / 100;
-    int newNumber = firstDigit * 10 - secondDigit;
-    newNumber = -newNumber;
-    Console.WriteLine($"средняя цифра числа -> {newNumber}");
-}
+// {
+//     int secondDigit = number / 10;
+//     int firstDigit = number / 100;
+//     int newNumber = firstDigit * 10 - secondDigit;
+//     newNumber = -newNumber;
+//     Console.WriteLine($"средняя цифра числа -> {newNumber}");
+// }
 
 // или через метод:
 {
