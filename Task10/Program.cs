5ab49c701ecf01d2413957790a15cc021034c8bb
Evgeniia <evgeniia.d@gmail.com>
2023-04-13 00:00:40 +0200

    добавила два решения

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -8,10 +8,29 @@
 
 Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number>=100&&number<=999)
+if (number >= 100 && number <= 999)
 {
-    int number = 
+    int secondDigit = number / 10;
+    int firstDigit = number / 100;
+    int newNumber = firstDigit * 10 - secondDigit;
+    newNumber = -newNumber;
+    Console.WriteLine($"средняя цифра числа -> {newNumber}");
 }
+
+// или через метод:
+{
+    int SecondDigit(int number)
+    {
+        int secondDigit = number / 10;
+        int firstDigit = number / 100;
+        int midDigit = firstDigit * 10 - secondDigit;
+        midDigit = -midDigit;
+        return midDigit;
+    }
+    int newNumber = SecondDigit(number);
+    Console.WriteLine($"вторая цифра числа -> {newNumber}");
+}
+
 else
 {
     Console.WriteLine("Вы ввели не трехзначное число");
