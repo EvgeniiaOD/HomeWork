4991942ef9a2471b89f685e97d02dd7f00ca49aa
Evgeniia <evgeniia.d@gmail.com>
2023-04-12 18:27:17 +0200

    добавила решение

---
+++ Task08/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая на вход
+// принимает число (N), а на выходе показывает все чётные
+// числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = 2;
+while (numberB <= numberA)
+{
+    if (numberB % 2 == 0)
+    {
+        Console.Write($"{numberB} ");
+    }
+    numberB++;
+}
