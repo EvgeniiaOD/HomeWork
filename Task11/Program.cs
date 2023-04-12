41a3b2f385b5c746c146aa428aabea0e18cc05d6
Evgeniia <evgeniia.d@gmail.com>
2023-04-12 23:01:52 +0200

    добавила комментарии и поправила код до рабочего

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -5,16 +5,16 @@
 // 782 -> 72
 // 918 -> 98
 
-int number = new Random() .Next(100,1000);
-Console.WriteLine ($"Случайное число из отрезка 100 - 999 -> {number}");
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка 100 - 999 -> {number}");
 
+int DeleteMiddle(int num)
 {
-    int DeleteMiddle(int num);
-int firstDigit = num / 100;
-int secondDigit = num % 10;
-int newNumber = firstDigit*10+secondDigit;
-return newNumber;
+    int firstDigit = num / 100;
+    int secondDigit = num % 10;
+    int newNumber = firstDigit * 10 + secondDigit;
+    return newNumber;
 }
-int numberB = DeleteMiddle (number);
-Console.WriteLine ($"Новое двойное число -> {numberB}");
-
+int numberB = DeleteMiddle(number);
+Console.WriteLine($"Двузначное число -> {numberB}"); // два варианта записи для вывода значения 
+Console.WriteLine("Двузначное число " + DeleteMiddle(number));
