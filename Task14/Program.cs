279bce14fa29fe977df1f23650baf3bd0ad9d89b
Evgeniia <evgeniia.d@gmail.com>
2023-04-15 18:46:43 +0200

    решение скопировала, но запись НЕ поняла

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -5,10 +5,12 @@
 // 46 -> нет 
 // 161 -> да 
 
+
+
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-bool result = MultipleTwoDigits(number, 5, 10);
+bool result = MultipleTwoDigits(number, 7, 23);
 Console.WriteLine(result ? "да" : "нет");
 
 bool MultipleTwoDigits(int num, int num1, int num2)
@@ -16,14 +18,25 @@ bool MultipleTwoDigits(int num, int num1, int num2)
     return num % num1 == 0 && num % num2 == 0;
 }
 
-Console.Write("Введите первое число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-bool result = IsSquare(number1, number2);
-Console.WriteLine(result ? "да" : "нет");
-bool IsSquare(int num1, int num2)
-{
-    return (num1 == num2 * num2) || (num2 == num1 * num1);
-}
+// bool result = MultipleTwoDigits(number, 5, 10);
+// Console.WriteLine(result ? "да" : "нет");
+
+// bool MultipleTwoDigits(int num, int num1, int num2)
+// {
+//     return num % num1 == 0 && num % num2 == 0;
+// }
+
+// Console.Write("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// bool result = IsSquare(number1, number2);
+// Console.WriteLine(result ? "да" : "нет");
+// bool IsSquare(int num1, int num2)
+// {
+//     return (num1 == num2 * num2) || (num2 == num1 * num1);
+// }
