8c61d8d3976af93f2b602ad33039f9130773cb7e
Evgeniia <evgeniia.d@gmail.com>
2023-04-16 16:07:05 +0200

    скопировала решение

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -15,3 +15,22 @@ string Quarter(int num)
 
 string result = Quarter(quarter);
 Console.WriteLine(result == null ? "такой четверти нет" : result); //если возвращается пустота, то говорим, что нет такой четверти, иначе...выдаем результат работы метода 
+
+// семинарское решение: 
+
+Console.WriteLine("Введите введите номер четверти");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+string range = Range(number);
+
+Console.WriteLine(range == null ? "Некорректный ввод": range);
+
+string Range(int num)
+{
+    if(num == 1) return "x > 0, y > 0";
+    if(num==2) return "x < 0, y > 0";
+    if(num == 3) return "x < 0, y < 0";
+    if(num==4) return "x > 0, y < 0";
+    return null;
+}
