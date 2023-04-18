87c725cd2adfeaa88cdb46292f23d705d391d492
Evgeniia <evgeniia.d@gmail.com>
2023-04-18 14:21:15 +0200

    добавила второй вариант решения

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -27,3 +27,30 @@ Console.WriteLine("Please enter array length: ");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] arr = CreateArray(size);
 PrintArray(arr);
+
+// второй вариант: 
+
+// Console.WriteLine("Введите длину массива");
+// int lenghtArray = Convert.ToInt32(Console.ReadLine());
+
+// int[] arr = CreateArray(lenghtArray);
+// PrintArray(arr);
+
+// int[] CreateArray(int size)
+// {
+//     int[] array = new int [size];
+//     Random random = new Random(); // оптимизация объекта Random (var - объект неявной типизации)
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = random.Next(0, 2);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+// }
