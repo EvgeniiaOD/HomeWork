e8e791a45b468aecda8b1afa2aa07aa583ab0006
Evgeniia <evgeniia.d@gmail.com>
2023-04-21 12:40:55 +0200

    скопировано решение

--- Task31/Program.cs
+++ Task31/Program.cs
@@ -31,3 +31,35 @@ Console.Write("[");
 PrintArray(array);
 Console.Write("]");
 Console.WriteLine();
+
+//
+
+int SumPositiveElements (int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0) sum += arr[i];
+    }
+    return sum;
+}
+
+int SumNegativeElements (int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] < 0) sum += arr[i];
+    }
+    return sum;
+}
+
+int[] array = CreateArrayRndInt(12, -9, 9);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+Console.WriteLine();
+int sumPositiveElem = SumPositiveElements(array);
+int sumNegativeElem = SumNegativeElements(array);
+Console.WriteLine($"Сумма положительных элементов = {sumPositiveElem}");
+Console.WriteLine($"Сумма отрицательных элементов = {sumNegativeElem}");
