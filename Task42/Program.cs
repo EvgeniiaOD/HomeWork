760c79edb7c1aba03697a2768c9bee5ac8578982
Evgeniia <evgeniia.d@gmail.com>
2023-04-25 13:29:40 +0200

    добавила второе решение

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -24,3 +24,26 @@ int DecToBin(int num)
     }
     return result;
 }
+
+
+// Console.Write("Введите целое положительное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if(number > 0)
+// {
+//     int resultBin = DecToBin(number);
+//     Console.WriteLine(resultBin);
+// }
+// else Console.WriteLine("вы ввели неверное число");
+
+// int DecToBin(int num)
+// {
+//     int count = 1;
+//     int result = 0;
+//     while (num > 0)
+//     {
+//         result += num % 2 * count;
+//         num /= 2;
+//         count = count * 10;
+//     }
+//     return result;
+// }
