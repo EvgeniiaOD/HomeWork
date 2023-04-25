385a97e923a31f15449b359ef67b06d9e378cbf5
Evgeniia <evgeniia.d@gmail.com>
2023-04-25 12:55:59 +0200

    добавила решение и комментарии

--- Task40/Program.cs
+++ Task40/Program.cs
@@ -3,3 +3,24 @@
 // такой длины.
 // Теорема о неравенстве треугольника: каждая сторона треугольника
 // меньше суммы двух других сторон.
+
+Console.WriteLine("Введите сторону A :");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону B :");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону C:");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+bool IsExistTriangle(int numA, int numB, int numC)
+{
+    // if (numA<numB+numC && numB<numA+numC && numC<numB+numA) 
+    return numA < numB + numC && numB < numA + numC && numC < numB + numA;
+    // return true;
+    // else return false;
+    // тут в ретёрн сразу передается результат, поэтому можем не использовать иф . 
+    // бул возвращает тру или фолс. поэтому можем сразу возвращать результат сравнения.
+    //  если тип будет не бул - то такой вариант уже не пройдет
+}
+
+bool existTriangle = IsExistTriangle(numberA, numberB, numberC);
+Console.WriteLine(existTriangle ? "такой треугольник существует" : "такой трейгольник НЕ существует");
--- Task42/Program.cs
+++ Task42/Program.cs
@@ -3,3 +3,7 @@
 // 45 -> 101101
 // 3 -> 11
 // 2 -> 10
+
+Console.WriteLine("Введите целое положительное число :");
+int number = Convert.ToInt32(Console.ReadLine());
+
