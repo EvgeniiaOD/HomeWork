238539af45ff40402c54cc51b9f7790e01f742bd
Evgeniia <evgeniia.d@gmail.com>
2023-05-15 16:08:56 +0200

    добавила решение, но поработать над округлением

---
+++ Task43/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine ("Введите знакчение k1: ");
+int k1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введите знакчение k2: ");
+int k2 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введите знакчение b1: ");
+int b1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введите знакчение b2: ");
+int b2 = Convert.ToInt32 (Console.ReadLine());
+
+
+// Console.WriteLine ($"{k1}{k2}{b1}{b2}");
+
+
+
---
+++ Task47/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreateTableRndDoub(int rows, int columns)
+{
+    double[,] table = new double[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = rnd.NextDouble(); 
+            Math.Round(table[i,j], 0, MidpointRounding.ToEven);
+        }
+    }
+    return table;
+}
+
+void PrintTable(double[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write($"{table[i, j],5} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] matrix = CreateTableRndDoub(3, 4);
+
+PrintTable(matrix);
