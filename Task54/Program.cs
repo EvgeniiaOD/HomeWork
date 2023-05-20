190ac9d237ec6686cbe0021cdf7c4a1e44e9a92b
Evgeniia <evgeniia.d@gmail.com>
2023-05-20 19:00:05 +0200

    доделать соритровку, правда где-то рядом

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -3,7 +3,65 @@
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
+
 // В итоге получается вот такой массив:
-// 7 4 2 1
-// 9 5 3 2
-// 8 4 4 2
+
+//       0 1 2 3          0 1 2 3 
+
+// 0     7 4 2 1      0   1 4 7 2
+// 1     9 5 3 2      1   5 9 2 3
+// 2     8 4 4 2      2   8 4 2 4
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] SortByRow(int[,] matrix)
+{
+    int[,] newMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int max = matrix[i, j];
+            int temp = 0;
+            if (matrix[i, j+1] > max)
+            // {
+            temp = matrix[i, j];
+            matrix [i,j] = matrix[i, j+1];
+            matrix[i, j+1] = temp;
+            newMatrix[i,j] = matrix[i, j];
+            // } 
+            // else j++;
+        }
+    }
+    return newMatrix;
+}
+
+int[,] matrix = CreateMatrixRndInt(4, 3, 0, 10);
+PrintMatrix(matrix);
+Console.WriteLine();
+// SortByRow(matrix);
+int[,] newMatrix = SortByRow(matrix);
+PrintMatrix(newMatrix);
