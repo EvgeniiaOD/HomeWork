0a30035fb893eb949771bc489da4ecd13ff057e3
Evgeniia <evgeniia.d@gmail.com>
2023-05-22 18:05:46 +0200

    добавила решение, можно улучшить, но не получается поменять два числа местами через функцию

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -6,93 +6,61 @@
 
 // В итоге получается вот такой массив:
 
-//       0 1 2 3          0 1 2 3 
+//       0 1 2 3     исходник->     0 1 2 3 
 
-// 0     7 4 2 1      0   1 4 7 2     4 7 2 
-// 1     9 5 3 2      1   5 9 2 3
-// 2     8 4 4 2      2   8 4 2 4
-
+// 0     7 4 2 1                0   1 4 7 2      
+// 1     9 5 3 2                1   5 9 2 3
+// 2     8 4 4 2                2   8 4 2 4
 int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
-    int[,] matrix = new int[rows, columns];
+    int[,] matr = new int[rows, columns];
     Random rnd = new Random();
 
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < matr.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < matr.GetLength(1); j++)
         {
-            matrix[i, j] = rnd.Next(min, max + 1);
+            matr[i, j] = rnd.Next(min, max + 1);
         }
     }
-    return matrix;
+    return matr;
 }
-void PrintMatrix(int[,] matrix)
+void PrintMatrix(int[,] matr)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < matr.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < matr.GetLength(1); j++)
         {
-            Console.Write($"{matrix[i, j],5} ");
+            Console.Write($"{matr[i, j],5} ");
         }
         Console.WriteLine();
     }
 }
-// void Swap(int coorI, int coorJ)
-// {
-//     int temp = 0;
-//     {
-//         temp = coorI;
-//         coorI = coorJ;
-//         coorJ = temp;
-//     }
-// }
 int[,] SortByRow(int[,] matr)
 {
     int[,] newMatr = new int[matr.GetLength(0), matr.GetLength(1)];
     for (int i = 0; i < matr.GetLength(0); i++)
-    {                            // 4
+    {
         for (int j = 1; j < matr.GetLength(1); j++)
         {
-            int count = 0;
-            while (count < matr.GetLength(1))
+            int k = j;
+            for (k = 0; k < matr.GetLength(1) - 1; k++)
             {
-
-                if (matr[i, j - 1] < matr[i, j])
+                if (matr[i, k] < matr[i, k + 1])
                 {
-                    int temp = matr[i, j - 1];
-                    matr[i, j - 1] = matr[i, j];
-                    matr[i, j] = temp;
+                    int temp = matr[i, k];
+                    matr[i, k] = matr[i, k + 1];
+                    matr[i, k + 1] = temp;
                 }
-                count++;
-
-
-
-                // int temp = matr[i, j - 1];
-                // matr[i, j - 1] = matr[i, j];
-                // matr[i, j] = temp;
-                // j++;
             }
-            // int max = matr[0, 0];
-            // if (matr[i, j] < matr[i, j + 1])
-            // if (max < matr[i, j + 1] & matr[i, j] < matr[i, j + 1])
-            // {
-            //     int temp = matr[i, j];
-            //     matr[i, j] = matr[i, j + 1];
-            //     matr[i, j + 1] = temp;
-            //     // Swap(matrix[i, j], matrix[i, j + 1]);
-            //     // newMatrix[i, j] = matrix[i, j];
-            // }
-            // j++;
         }
     }
-    // return newMatrix;
     return matr;
 }
+
 
 int[,] matrix = CreateMatrixRndInt(4, 3, 0, 20);
 PrintMatrix(matrix);
 Console.WriteLine();
 int[,] newMatrix = SortByRow(matrix);
-// int[,] newMatrix = SortByRow(matrix);
-// PrintMatrix(newMatrix);
 PrintMatrix(newMatrix);
