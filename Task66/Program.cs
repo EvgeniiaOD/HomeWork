9e1536b3ffafc55c4fa83d4fe0c7d90910a15f3e
Evgeniia <evgeniia.d@gmail.com>
2023-05-24 00:14:38 +0200

    добавила решение

---
+++ Task66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите целое положительное число");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое положительное число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int SumNaturalNumbers(int numM, int numN) // если М меньше N
+{
+    if (numM == numN ) return numN; 
+    return numM + SumNaturalNumbers(numM + 1, numN);
+}
+int SumNaturalNumbers2(int numM, int numN) // если M больше N
+{
+    if (numM == numN) return numN; // 8
+    return numM + SumNaturalNumbers2(numM - 1, numN);
+}
+
+if (numberM> numberN) Console.WriteLine(SumNaturalNumbers2(numberM, numberN));
+else Console.WriteLine(SumNaturalNumbers(numberM, numberN));
